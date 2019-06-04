515f89c231f6eabb8e3e1b3304068f519cc1eeb7
yiphoipan <yiphoipan@yiphoipandembp.mynet>
2019-06-04 21:37:47 +0800

    添加进度条功能

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -39,43 +39,43 @@ $(function () {
         }
     });
 
-    $('#form').on("success.form.bv", function( e ) {
-        // 阻止默认的表单提交
-        e.preventDefault();
-
-        //console.log( "校验成功后的 表单提交 被阻止了" );
-
-        // 通过 ajax 进行提交
-        $.ajax({
-            type: "post",
-            url: "/employee/employeeLogin",
-            data: $('#form').serialize(),
-            dataType: "json",
-            success: function( info ) {
-                console.log( info );
-                if ( info.success ) {
-                    // 登录成功, 跳转到首页
-                    location.href = "index.html";
-                }
-
-                if ( info.error === 1000 ) {
-                    //alert("当前用户名不存在");
-                    // updateStatus 更新校验状态
-                    // 1. 字段名称
-                    // 2. 校验状态  VALID, INVALID   NOT_VALIDATED未校验的,  VALIDATING校验中的
-                    // 3. 校验规则, 用于指定提示文本
-                    $('#form').data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
-                }
-
-                if ( info.error === 1001 ) {
-                    //alert("密码错误");
-
-                    $('#form').data("bootstrapValidator").updateStatus("password", "INVALID", "callback");
-                }
-            }
-        })
-
-    });
+    // $('#form').on("success.form.bv", function( e ) {
+    //     // 阻止默认的表单提交
+    //     e.preventDefault();
+    //
+    //     //console.log( "校验成功后的 表单提交 被阻止了" );
+    //
+    //     // 通过 ajax 进行提交
+    //     $.ajax({
+    //         type: "post",
+    //         // url: "/employee/employeeLogin",
+    //         data: $('#form').serialize(),
+    //         dataType: "json",
+    //         success: function( info ) {
+    //             console.log( info );
+    //             if ( info.success ) {
+    //                 // 登录成功, 跳转到首页
+    //                 location.href = "index.html";
+    //             }
+    //
+    //             if ( info.error === 1000 ) {
+    //                 //alert("当前用户名不存在");
+    //                 // updateStatus 更新校验状态
+    //                 // 1. 字段名称
+    //                 // 2. 校验状态  VALID, INVALID   NOT_VALIDATED未校验的,  VALIDATING校验中的
+    //                 // 3. 校验规则, 用于指定提示文本
+    //                 $('#form').data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
+    //             }
+    //
+    //             if ( info.error === 1001 ) {
+    //                 //alert("密码错误");
+    //
+    //                 $('#form').data("bootstrapValidator").updateStatus("password", "INVALID", "callback");
+    //             }
+    //         }
+    //     })
+    //
+    // });
 
     $('[type="reset"]').click(function () {
         $('#form').data('bootstrapValidator').resetForm();
